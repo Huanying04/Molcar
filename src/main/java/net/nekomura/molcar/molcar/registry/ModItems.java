9b4c13cb604029f6aefc0082c8c03506d79e016a
Huanying04 <ken911230@gmail.com>
2021-02-14 11:30:58 +0800

    修改生菜材質及ID

--- src/main/java/net/nekomura/molcar/molcar/registry/ModItems.java
+++ src/main/java/net/nekomura/molcar/molcar/registry/ModItems.java
@@ -1,6 +1,5 @@
 package net.nekomura.molcar.molcar.registry;
 
-import net.minecraft.entity.EntityType;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -11,11 +10,11 @@ import net.nekomura.molcar.molcar.Molcar;
 
 public class ModItems {
 
-    public static final Item LETTUCE = new Item(new Item.Settings().group(ItemGroup.FOOD).maxCount(64).food(new FoodComponent.Builder().hunger(2).snack().build()));
+    public static final Item LETTUCE_LEAF = new Item(new Item.Settings().group(ItemGroup.FOOD).maxCount(64).food(new FoodComponent.Builder().hunger(2).snack().build()));
     public static final Item MOLCAR_SPAN_EGG = new SpawnEggItem(ModEntities.MOLCAR, 16641740, 16762963, (new Item.Settings()).group(ItemGroup.MISC));
 
     public static void register() {
-        Registry.register(Registry.ITEM, new Identifier(Molcar.MOD_ID, "lettuce"), LETTUCE);
+        Registry.register(Registry.ITEM, new Identifier(Molcar.MOD_ID, "lettuce_leaf"), LETTUCE_LEAF);
         Registry.register(Registry.ITEM, new Identifier(Molcar.MOD_ID, "molcar_spawn_egg"), MOLCAR_SPAN_EGG);
     }
 
